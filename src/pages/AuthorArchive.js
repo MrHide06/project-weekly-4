894e397621194ffcf9590b1751144b66ffe78ddd
zakiydani <zakiydani87@gmail.com>
2020-04-11 17:55:34 +0700

    Update

---
+++ src/pages/AuthorArchive.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Jumbotron, Container } from 'react-bootstrap'
+
+
+
+const AuthorArchive = () => {
+    return (
+        <div>
+            <div>
+                <Jumbotron>
+                    <Container>
+                        <h1 className="h1Tag">AUTHOR ARCHIVE</h1>
+                    </Container>
+                </Jumbotron>
+            </div>
+        </div>
+    )
+}
